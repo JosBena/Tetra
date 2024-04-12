03a7c10c52ac2d1f3572fa0934a9556a2c945f3a
JosBena <162043587+JosBena@users.noreply.github.com>
2024-04-12 17:24:37 -0400

    added lfs and gitattributes

--- Assets/_Scripts/Board.cs
+++ Assets/_Scripts/Board.cs
@@ -7,6 +7,16 @@ public class Board : MonoBehaviour
     public Piece activePice { get; private set; }
     public TetrominoData[] tetrominoes;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -41,11 +51,31 @@ public class Board : MonoBehaviour
         }
     }
 
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
     public bool IsValidPosition(Piece piece, Vector3Int position)
     {
+        RectInt bounds = this.Bounds;
         for (var i = 0; i < piece.cells.Length; i++)
         {
             Vector3Int tilePosition = piece.cells[i] + position; // 47:52
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
         }
         return true;
     }
--- Assets/_Scripts/Piece.cs
+++ Assets/_Scripts/Piece.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Piece : MonoBehaviour
 {
@@ -7,12 +8,14 @@ public class Piece : MonoBehaviour
     public TetrominoData data { get; private set; }
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
 
     public void Initalize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
+        this.rotationIndex = 0;
 
         if (this.cells == null)
         {
@@ -27,6 +30,17 @@ public class Piece : MonoBehaviour
 
     private void Update()
     {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(+1);
+        }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             Move(Vector2Int.left);
@@ -35,11 +49,86 @@ public class Piece : MonoBehaviour
         {
             Move(Vector2Int.right);
         }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        this.board.Set(this);
     }
 
-    private void Move(Vector2Int translation)
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    private bool Move(Vector2Int translation)
     {
         Vector3Int newPosition = this.position;
-        newPosition.Set(newPosition.x + translation.x, newPosition.y + translation.y, 0);
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        //newPosition.Set(newPosition.x + translation.x, newPosition.y + translation.y, 0);
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+        }
+        return valid;
+    }
+
+    private void Rotate(int direction) //1:11:04
+    {
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4); //0 and 4 since there is only 4 posible rotations
+
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    //math function that wraps around
+    //-1 0 2
+    // 2 - (0-(-1)) % (2-0) =  2 - 1%2 = 2-1 = 1
+    //3 0 2
+    //0 + (3-0) % (2-0) = 0 + 3%2 = 0 + 1 = 1
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
     }
 }
